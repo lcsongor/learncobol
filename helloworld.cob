46fcf5e857ba62c454922297ad107f2ad47c3ba9
Lengyel Csongor <lcsongor@gmail.com>
2025-11-01 10:22:38 +0100

    Further progress with first program

--- helloworld.cob
+++ helloworld.cob
@@ -3,9 +3,14 @@
        DATA DIVISION.
        WORKING-STORAGE SECTION.
        01 USER-NAME PIC A(30).
+       01 AGE1 PIC 99.
        PROCEDURE DIVISION.
            DISPLAY "Enter your name: ".
            ACCEPT USER-NAME.
-           DISPLAY "Hello, " USER-NAME "!".
+           DISPLAY "Enter age:".
+           ACCEPT AGE1.
+           DISPLAY "Hello, " FUNCTION TRIM(USER-NAME TRAILING)
+                   " with " AGE1 " years old !".
            DISPLAY "Welcome to COBOL on Windows 11.".
+           DISPLAY "This is a simple COBOL program.".
            STOP RUN.
